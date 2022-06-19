75b0713b9b846992715444d9cf7a1e7e98146822
Nam <dangtrannam2001@gmail.com>
2022-06-19 15:36:17 +0700

    create lesson validation pipes

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -1,3 +1,4 @@
+import { CreateLessonInput } from './lesson.input';
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -19,7 +20,8 @@ export class LessonService {
     }
   }
 
-  async createLesson(name, startDate, endDate): Promise<Lesson> {
+  async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
+    const { name, startDate, endDate } = createLessonInput;
     try {
       const lesson = this.lessonRepository.create({
         id: uuid(),
